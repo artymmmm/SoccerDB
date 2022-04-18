c853f3a0705a35bdacdf41b90e7fd3d3dcab86c1
Artem <arty1303@gmail.com>
2022-04-18 13:41:49 +0300

    Added The end

---
+++ Soccer/Soccer/Player.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Soccer
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public int Age { get; set; }
+        public int? TeamId { get; set; }
+        public virtual Team Team { get; set; }
+
+    }
+    public class SoccerContext : DbContext
+    {
+        public SoccerContext()
+            : base("DefaultConnection")
+        { }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Team> Teams { get; set; }
+    }
+    public class Team
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Coach { get; set; }
+        public virtual ICollection<Player> Players { get; set; } 
+    }
+}
---
+++ Soccer/Soccer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Soccer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SoccerContext db = new SoccerContext())
+            {
+                var p = db.Players.ToList();
+                foreach (var player in p)
+                {
+                    Console.WriteLine("{0} - {1}", player.Name, player.Team.Name);
+                }
+            }
+                }
+            }
+        }
